89d056f73a9d6b55cf912aa6cd6ac1f5d4367f31
pooja-chaudhary <poojachaudhary831@gmail.com>
2022-08-27 13:49:09 +0530

    Add files via upload

---
+++ precache-manifest.6d113fa1181d1b9ae70a1735eb233aa8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95623bbfbdfe0dfa67ce75ebf53c1188",
+    "url": "/react-components-and--properties/index.html"
+  },
+  {
+    "revision": "bc816f983a6680963541",
+    "url": "/react-components-and--properties/static/js/2.09df5970.chunk.js"
+  },
+  {
+    "revision": "05e013439ffd2fb554a8",
+    "url": "/react-components-and--properties/static/js/main.b1801121.chunk.js"
+  },
+  {
+    "revision": "b8f9d878bd62068a8eed",
+    "url": "/react-components-and--properties/static/js/runtime-main.bd5831ed.js"
+  }
+]);
